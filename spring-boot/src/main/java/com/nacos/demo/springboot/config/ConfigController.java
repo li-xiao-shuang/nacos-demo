c4856cb8df2da6d75abd67753386e98959ce74cf
lixiaoshuang <644968328@qq.com>
2021-11-12 17:38:53 +0800

    添加config动态刷新操作

--- spring-boot/src/main/java/com/nacos/demo/springboot/config/ConfigController.java
+++ spring-boot/src/main/java/com/nacos/demo/springboot/config/ConfigController.java
@@ -18,20 +18,45 @@ package com.nacos.demo.springboot.config;
 
 import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Executor;
+
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME;
 
 /**
  * @author lixiaoshuang
  */
 @RestController
 @RequestMapping("springboot/config")
+@NacosPropertySource(name = "custom", dataId = "user.info", first = true, before = SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME, after = SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME)
 public class ConfigController {
     
     @NacosInjected
     private ConfigService configService;
+    
+    /**
+     * dataId : user.info content: name = hh age = 22
+     */
+    @NacosValue(value = "${name}", autoRefreshed = true)
+    private String name;
+    
+    @NacosValue(value = "${age}", autoRefreshed = true)
+    private String age;
+    
+    @Autowired
+    private UserInfoConfig userInfoConfig;
+    
+    @Autowired
+    private UserInfoConfig2 userInfoConfig2;
     
     
     /**
@@ -76,4 +101,47 @@ public class ConfigController {
             throws NacosException {
         return configService.removeConfig(dataId, group);
     }
+    
+    
+    /**
+     * 监听配置信息
+     *
+     * @param dataId
+     * @param group
+     * @throws NacosException
+     */
+    @RequestMapping("listener")
+    public void listenerConfig(@RequestParam("dataId") String dataId, @RequestParam("group") String group)
+            throws NacosException {
+        configService.addListener(dataId, group, new Listener() {
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+            
+            @Override
+            public void receiveConfigInfo(String configInfo) {
+                System.out.println("监听配置：" + configInfo);
+            }
+        });
+    }
+    
+    /**
+     * 获取自动刷新配置 需要使用 @NacosPropertySource 注解配合@NacosValue来使用
+     *
+     * @return
+     */
+    @RequestMapping("userInfo")
+    public void getUserInfo() {
+        //方式1
+        System.out.println("方式1 name: " + name);
+        System.out.println("方式1 age: " + age);
+        //方式2
+        System.out.println("方式2 name: " + userInfoConfig.getName());
+        System.out.println("方式2 age: " + userInfoConfig.getAge());
+        //方式3
+        System.out.println("方式3 name " + userInfoConfig2.getName());
+        System.out.println("方式3 age: " + userInfoConfig2.getAge());
+    }
+    
 }
---
+++ spring-boot/src/main/java/com/nacos/demo/springboot/config/UserInfoConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 Gypsophila open source organization.
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nacos.demo.springboot.config;
+
+import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME;
+
+/**
+ * @author lixiaoshuang
+ */
+@NacosPropertySource(name = "custom", dataId = "user.info", first = true, before = SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME, after = SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME)
+@Data
+@Component
+public class UserInfoConfig {
+    
+    @NacosValue(value = "${name}", autoRefreshed = true)
+    private String name;
+    
+    @NacosValue(value = "${age}", autoRefreshed = true)
+    private int age;
+}
